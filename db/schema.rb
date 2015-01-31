bf217aa5f523f644022c007a4f8a778e7a4ac021
Matt May <matthewbentonmay@gmail.com>
2015-01-31 10:55:07 -0600

    added migration for answers

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150131162527) do
+ActiveRecord::Schema.define(version: 20150131165444) do
 
   create_table "answers", force: true do |t|
     t.text     "description"
     t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "correct"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id"
