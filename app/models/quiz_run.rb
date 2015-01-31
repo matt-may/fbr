3c6476511791fdca02a9c994aabb177bb02070e1
Matt May <matthewbentonmay@gmail.com>
2015-01-30 19:19:39 -0600

    updated schema and defined associations

--- app/models/quiz_run.rb
+++ app/models/quiz_run.rb
@@ -1,2 +1,4 @@
 class QuizRun < ActiveRecord::Base
+  has_many :questions
+  has_many :results
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,6 @@
 class Result < ActiveRecord::Base
   belongs_to :question
   belongs_to :quiz_run
+  belongs_to :expected_answer, class_name: 'Answer', foreign_key: 'expected_answer_id'
+  belongs_to :actual_answer, class_name: 'Answer', foreign_key: 'actual_answer_id'
 end
